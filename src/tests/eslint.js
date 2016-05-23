8cbbc2f88bd4f66005b6614023729e5c0ff51acc
Brooks Newberry <mail@brooks.is>
2016-05-23 16:24:34 -0700

    create npm test script, setup travis-ci

---
+++ src/tests/eslint.js
@@ -0,0 +1,4 @@
+import test from 'tape'
+import eslint from 'tape-eslint'
+
+test('eslint', eslint())
---
+++ src/tests/iocupid.js
@@ -0,0 +1,5 @@
+import test from 'tape'
+test('welcome', t => {
+  t.plan(1)
+  t.ok(true, 'Write tests for new features with tape.')
+})
