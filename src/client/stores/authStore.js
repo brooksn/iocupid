633aad7aca274b6279a19c1938465fd15bf0ae65
Brooks Newberry <mail@brooks.is>
2016-05-29 17:59:20 -0700

    add authStore

---
+++ src/client/stores/authStore.js
@@ -0,0 +1,48 @@
+import throttle from 'lodash.throttle'
+import decode from 'jwt-decode'
+import EventEmitter from 'events'
+const authStore = new EventEmitter()
+const localStorageKey = 'base64jwt'
+export const CHANGE_EVENT = 'change'
+
+const getLocalStore = function getLocalStore() {
+  let base64jwt = null
+  if (window) {
+    base64jwt = window.localStorage.getItem(localStorageKey)
+  }
+  return {base64jwt}
+}
+
+const store = getLocalStore()
+
+const saveStoreToLocalStorage = function saveStoreToLocalStorage() {
+  if (window) window.localStorage.setItem(localStorageKey, store.base64jwt)
+}
+
+const unthrottledEmitChange = function unthrottledEmitChange() {
+  saveStoreToLocalStorage()
+  authStore.emit(CHANGE_EVENT)
+}
+const emitChange = throttle(unthrottledEmitChange, 200)
+
+export const setJWTBase64 = function setJWTBase64(token) {
+  if (typeof token === 'string') store.base64jwt = token
+  else throw Error('Type of token must be a string.')
+  emitChange(CHANGE_EVENT)
+}
+
+export const getJWTPayload = function getJWT() {
+  if (!store.base64jwt) return store.base64jwt
+  else return decode(store.base64jwt)
+}
+
+export const getJWTBase64 = function getJWTBase64() {
+  return store.base64jwt
+}
+
+export const getUserID = function getUserID() {
+  if (!store.base64jwt) return store.base64jwt
+  else return decode(store.base64jwt).userID
+}
+
+export default authStore
