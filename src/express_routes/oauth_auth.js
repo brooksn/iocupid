899d4c395d1ac677923bd13d339387f4c4cf95dd
Brooks Newberry <mail@brooks.is>
2016-06-01 16:32:20 -0700

    Change the JWT payload

--- src/express_routes/oauth_auth.js
+++ src/express_routes/oauth_auth.js
@@ -17,28 +17,35 @@ module.exports = function* oauth_auth(code) {
   })
   const tokenJson = yield tokenRes.json()
   const token = tokenJson.access_token
+  const ghUserUrl = `https://api.github.com/user?access_token=${token}`
   const ghEmailsUrl = `https://api.github.com/user/emails?access_token=${token}`
-  const ghEmails = yield fetch(ghEmailsUrl).then(res => res.json())
+  const ghUserReq = fetch(ghUserUrl).then(res => res.json())
+  const ghEmailsReq = fetch(ghEmailsUrl).then(res => res.json())
+  const responses = yield Promise.all([ghUserReq, ghEmailsReq])
+  const ghUsername = responses[0].login
+  if (typeof ghUsername !== 'string') throw new Error(ghUsername)
+  const ghEmails = responses[1]
   const addresses = ghEmails.map(e => e.email)
   if (addresses.length === 0) throw Error('No email addresses are associated with this user.')
   const getUserId = userIdWithEmail.bind(this)
   let userID = yield getUserId(addresses).then(cursor => cursor.next()).catch(() => null)  
-  const ioEmails = ghEmails.map(email => {
-    email.primary = email.primary === 'primary' ? 'github' : null
-    return email
+  const ioEmails = ghEmails.map(ghEmail => {
+    const e = {email: ghEmail.email}
+    if (ghEmail.primary === true) e.primary = true
+    return e
   })
+  const rethinkRow = {emails: ioEmails, services: {github: {token, username: ghUsername}}}
   if (!userID) {
     userID = nonce(6)
-    yield r.db(this.rdbname).table(this.utable)
-    .insert({id: userID, emails: ioEmails, tokens: {github: token}}, {returnChanges: true})
-    .run(this.rconn)
+    rethinkRow.id = userID
+    var res = yield r.db(this.rdbname).table(this.utable)
+    .insert(rethinkRow, {returnChanges: true}).run(this.rconn)
   } else {
-    yield r.db(this.rdbname).table(this.utable).get(userID)
-    .update({emails: ioEmails, tokens: {github: token}}, {returnChanges: true})
-    .run(this.rconn)
+    res = yield r.db(this.rdbname).table(this.utable).get(userID)
+    .update(rethinkRow, {returnChanges: true}).run(this.rconn)
   }
-  // userID = user.changes[0].new_val.id
-  const payload = {userID}
+  const user = res.changes[0].new_val
+  const payload = {userID, services: user.services}
   const jwt = jsonwebtoken.sign(payload, JWT_SECRET)
   return jwt
 }
