465c2f1076ed0a57d48c7665e1577b58b1adf2b7
Brooks Newberry <mail@brooks.is>
2016-06-01 16:33:43 -0700

    Child components receive JWT payload in props

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 import GitHubForkRibbon from 'react-github-fork-ribbon'
+import store, { getJWTPayload, CHANGE_EVENT } from '../stores/authStore.js'
 import Header from './Header.jsx'
 
 export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      jwtPayload: getJWTPayload()
+    }
+  }
   render() {
+    const childrenWithProps = React.Children.map(this.props.children,
+      child => React.cloneElement(child, {
+          jwtPayload: this.state.jwtPayload || null
+        })
+      )
     return (
     <div className="app" thingy={true}>
       <GitHubForkRibbon
@@ -13,13 +25,22 @@ export class App extends Component {
         Fork me on GitHub
       </GitHubForkRibbon>
       <div style={{marginLeft:'0.4em', marginRight:'0.4em'}}>
-        <Header />
+        <Header jwtPayload={this.state.jwtPayload} />
         <div className="container">
-          {this.props.children}
+          {childrenWithProps}
         </div>
       </div>
     </div>
     )
+  }
+  authStoreChange() {
+    this.setState({jwtPayload: getJWTPayload()})
+  }
+  componentDidMount() {
+    store.on(CHANGE_EVENT, this.authStoreChange.bind(this))
+  }
+  componentWillUnmount() {
+    store.removeListener(CHANGE_EVENT, this.authStoreChange.bind(this))
   }
 }
 
