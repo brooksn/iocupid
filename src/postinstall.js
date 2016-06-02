47dece9d5bdc1fab9616cd194709437f05229b8b
Brooks Newberry <mail@brooks.is>
2016-06-01 22:33:13 -0700

    Divert postinstall script in Travis CI

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,11 +1,15 @@
 require('dotenv-safe').config({silent: true})
-console.log('Starting postinstall.js') //eslint-disable-line
-const co = require('co')
-const setupDatabase = require('./setupDatabase.js').bind(this)
-const rconnectionparams = { host: 'localhost', port: 28015 }
-this.rdbname = 'iocupid'
-this.utable = 'users'
-co(setupDatabase(rconnectionparams))
-.then(rconn => {
-  rconn.close({noreplyWait: false})
-})
+if (!process.env.TRAVIS) {
+  console.log('Starting postinstall.js') //eslint-disable-line
+  const co = require('co')
+  const setupDatabase = require('./setupDatabase.js').bind(this)
+  const rconnectionparams = { host: 'localhost', port: 28015 }
+  this.rdbname = 'iocupid'
+  this.utable = 'users'
+  co(setupDatabase(rconnectionparams))
+  .then(rconn => {
+    rconn.close({noreplyWait: false})
+  })
+} else {
+  console.log('Skipping postinstall because TRAVIS=true') //eslint-disable-line
+}
